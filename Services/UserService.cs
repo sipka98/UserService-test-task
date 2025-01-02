8fe94827e8180877f3d678a1d7ef3ef510a38a57
sipka98 <jovan.sipkaa@gmail.com>
2025-01-02 17:08:09 +0100

    namespace Services

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -1,61 +1,64 @@
-public class UserService
-{
-    private readonly ValidationService _validationService;
+namespace Services {
 
-    public UserService(ValidationService validationService)
+    public class UserService
     {
-        _validationService = validationService;
-    }
+        private readonly ValidationService _validationService;
 
-    public void CreateUser(string name, string email, string password, string role)
-    {
-        _validationService.ValidateName(name);
-        _validationService.ValidateEmail(email);
-        _validationService.ValidatePassword(password);
-        _validationService.ValidateRole(role);
-
-        var passwordHash = BCrypt.Net.BCrypt.HashPassword(password);
-
-        using (var db = new SqlConnection("connectionString"))
+        public UserService(ValidationService validationService)
         {
-            db.Open();
-            var command = new SqlCommand(
-                $"INSERT INTO Users (Name, Email, PasswordHash, Role) VALUES ('{name}', '{email}', '{passwordHash}', '{role}')", 
-                db
-            );
-            command.ExecuteNonQuery();
+            _validationService = validationService;
         }
-    }
 
-    public List<string> GetUsers()
-    {
-        var users = new List<string>();
-
-        using (var db = new SqlConnection("connectionString"))
+        public void CreateUser(string name, string email, string password, string role)
         {
-            db.Open();
-            var command = new SqlCommand("SELECT Name FROM Users", db);
-            using (var reader = command.ExecuteReader())
+            _validationService.ValidateName(name);
+            _validationService.ValidateEmail(email);
+            _validationService.ValidatePassword(password);
+            _validationService.ValidateRole(role);
+
+            var passwordHash = BCrypt.Net.BCrypt.HashPassword(password);
+
+            using (var db = new SqlConnection("connectionString"))
             {
-                while (reader.Read())
-                {
-                    users.Add(reader.GetString(0));
-                }
+                db.Open();
+                var command = new SqlCommand(
+                    $"INSERT INTO Users (Name, Email, PasswordHash, Role) VALUES ('{name}', '{email}', '{passwordHash}', '{role}')", 
+                    db
+                );
+                command.ExecuteNonQuery();
             }
         }
 
-        return users;
-    }
-
-    public void UpdateUserRole(int userId, string newRole)
-    {
-        _validationService.ValidateRole(newRole);
-
-        using (var db = new SqlConnection("connectionString"))
+        public List<string> GetUsers()
         {
-            db.Open();
-            var command = new SqlCommand($"UPDATE Users SET Role = '{newRole}' WHERE Id = {userId}", db);
-            command.ExecuteNonQuery();
+            var users = new List<string>();
+
+            using (var db = new SqlConnection("connectionString"))
+            {
+                db.Open();
+                var command = new SqlCommand("SELECT Name FROM Users", db);
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        users.Add(reader.GetString(0));
+                    }
+                }
+            }
+
+            return users;
+        }
+
+        public void UpdateUserRole(int userId, string newRole)
+        {
+            _validationService.ValidateRole(newRole);
+
+            using (var db = new SqlConnection("connectionString"))
+            {
+                db.Open();
+                var command = new SqlCommand($"UPDATE Users SET Role = '{newRole}' WHERE Id = {userId}", db);
+                command.ExecuteNonQuery();
+            }
         }
     }
 }
